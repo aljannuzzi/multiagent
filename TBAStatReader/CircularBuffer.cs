688a995d1f2778f413dbbdb1931e8f9f27c5106a
Brandon🌩️H <bc3tech@users.noreply.github.com>
2024-04-07 21:01:32 -0700

    Adding progress indicator

---
+++ TBAStatReader/CircularBuffer.cs
@@ -0,0 +1,17 @@
+﻿namespace TBAStatReader
+{
+    internal class CircularCharArray(params char[] content)
+    {
+        private int _pos = 0;
+
+        public char Next()
+        {
+            if (++_pos >= content.Length)
+            {
+                _pos = 0;
+            }
+
+            return content[_pos];
+        }
+    }
+}
