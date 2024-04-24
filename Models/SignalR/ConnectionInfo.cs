c69e141224271588b43abfb6bbed569481a1ef70
Brandon🌩️H <bc3tech@users.noreply.github.com>
2024-04-24 09:50:36 -0700

    simplify

--- Models/SignalR/ConnectionInfo.cs
+++ Models/SignalR/ConnectionInfo.cs
@@ -2,17 +2,7 @@
 /// <summary>
 /// Contains necessary information for a SignalR client to connect to SignalR Service.
 /// </summary>
-public sealed class ConnectionInfo
+public sealed record ConnectionInfo(Uri Url, string? AccessToken)
 {
-    /// <summary>
-    /// The URL for a client to connect to SignalR Service.
-    /// </summary>
-    public string Url { get; set; }
-
-    /// <summary>
-    /// The access token for a client to connect to SignalR service.
-    /// </summary>
-    public string AccessToken { get; set; }
-
     public Task<string?> GetAccessToken() => Task.FromResult(this.AccessToken);
 }
