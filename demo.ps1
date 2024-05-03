da6aaf92765a9b80d97b70639ecba2041efdde1b
Brandon🌩️H <bc3tech@users.noreply.github.com>
2024-05-03 16:24:14 -0700

    Demo script

---
+++ demo.ps1
@@ -0,0 +1,31 @@
+Write-Host "Starting hub..."
+./launch.ps1 signalrhub
+Start-Sleep -Seconds 5
+
+Write-Host "Starting Router/Orchestrator..."
+./launch.ps1 orchestrator
+Start-Sleep -Seconds 2
+
+Write-Host "Starting User client..."
+./launch.ps1 client
+Start-Sleep -Seconds 2
+
+Write-Host "Starting Agent 1..."
+./launch.ps1 districtsagent
+
+Write-Host "Press <Enter> to continue to the next stage"
+while ([System.Console]::ReadKey($true).Key -ne "Enter") {
+    # Wait for Enter key
+}
+
+Write-Host "Starting Agent 2..."
+./Launch teamsagent
+
+Write-Host "Press <Enter> to continue to the next stage"
+while ([System.Console]::ReadKey($true).Key -ne "Enter") {
+    # Wait for Enter key
+}
+
+Write-Host "Starting Other agents..."
+./Launch eventsagent
+./Launch matchesagent
