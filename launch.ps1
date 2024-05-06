6b1ddfc1bfe21740d52481a1c96be6aa24c80804
Brandon🌩️H <bc3tech@users.noreply.github.com>
2024-05-06 14:53:56 -0700

    Reorganizing build & deploy scripts

--- launch.ps1
+++ launch.ps1
@@ -2,4 +2,4 @@ param(
 	[string]$module
 )
 
-Start-Process -WorkingDirectory "./build/$module" -FilePath (Get-ChildItem -Path "./build/$module" -Filter "*.exe" -Depth 1 | Select-Object -First 1).FullName
+Start-Process -WorkingDirectory "./o/$module" -FilePath (Get-ChildItem -Path "./o/$module" -Filter "*.exe" -Depth 1 | Select-Object -First 1).FullName
