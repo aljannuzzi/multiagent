76ecf6552c23baac8ae87abbd4f8ab84ff30f1eb
Brandon🌩️H <bc3tech@users.noreply.github.com>
2024-05-08 08:23:28 -0700

    fixing pathing in the launch script

--- launch.ps1
+++ launch.ps1
@@ -2,4 +2,4 @@ param(
 	[string]$module
 )
 
-Start-Process -WorkingDirectory "./o/$module" -FilePath (Get-ChildItem -Path "./o/$module" -Filter "*.exe" -Depth 1 | Select-Object -First 1).FullName
+Start-Process -WorkingDirectory "$PSScriptRoot/o/$module" -FilePath (Get-ChildItem -Path "$PSScriptRoot/o/$module" -Filter "*.exe" -Depth 1 | Select-Object -First 1).FullName
